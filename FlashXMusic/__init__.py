2a6ab6413aae6bc62d099c7e02c4049d77ab3c0d
FlashXMusic <emoji5525522@gmail.com>
2024-08-01 09:00:20 +0630

    Add files via upload

---
+++ FlashXMusic/__init__.py
@@ -0,0 +1,30 @@
+from SafoneAPI import SafoneAPI
+
+from FlashXMusic.core.bot import VIP
+from FlashXMusic.core.dir import dirr
+from FlashXMusic.core.git import git
+from FlashXMusic.core.userbot import Userbot
+from FlashXMusic.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = VIP()
+api = SafoneAPI()
+userbot = Userbot()
+HELPABLE = {}
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+APP = "tg_vc_bot"  # connect music api key "Dont change it"
