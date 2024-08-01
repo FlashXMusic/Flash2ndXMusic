a4007d007c74c434b3ee9c494360e15cd3108fb2
FlashXMusic <emoji5525522@gmail.com>
2024-08-01 17:32:09 +0630

    Update welcome.py

--- VIPMUSIC/cplugin/welcome.py
+++ VIPMUSIC/cplugin/welcome.py
@@ -1,15 +1,17 @@
 from logging import getLogger
 
 from PIL import Image, ImageChops, ImageDraw, ImageEnhance, ImageFont
-from pyrogram import Client, enums, filters
+from pyrogram import enums, filters
 from pyrogram.types import ChatMemberUpdated, InlineKeyboardButton, InlineKeyboardMarkup
 
+from VIPMUSIC import app
+
 random_photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://telegra.ph/file/c1e44824e6b8d41def80c.jpg",
+    "https://telegra.ph/file/18e61e800e05062b61b83.jpg",
+    "https://telegra.ph/file/0968f0f08b94cc21c2689.jpg",
+    "https://telegra.ph/file/9e56aec423cd7c5daabf9.jpg",
+    "https://telegra.ph/file/0b98dc047825876744a2e.jpg",
 ]
 # --------------------------------------------------------------------------------- #
 
@@ -45,109 +47,40 @@ class temp:
     B_NAME = None
 
 
-def circle(pfp, size=(500, 500), brightness_factor=10):
-    pfp = pfp.resize(size, Image.ANTIALIAS).convert("RGBA")
-    pfp = ImageEnhance.Brightness(pfp).enhance(brightness_factor)
+def circle(pfp, size=(700, 700)):
+    pfp = pfp.resize(size, Image.LANCZOS).convert("RGBA")
     bigsize = (pfp.size[0] * 3, pfp.size[1] * 3)
     mask = Image.new("L", bigsize, 0)
     draw = ImageDraw.Draw(mask)
     draw.ellipse((0, 0) + bigsize, fill=255)
-    mask = mask.resize(pfp.size, Image.ANTIALIAS)
+    mask = mask.resize(pfp.size, Image.LANCZOS)
     mask = ImageChops.darker(mask, pfp.split()[-1])
     pfp.putalpha(mask)
     return pfp
 
-
-def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
-    background = Image.open("VIPMUSIC/assets/wel2.png")
+def welcomepic(pic, user, chatname, id, uname):
+    background = Image.open("VIPMUSIC/assets/VIPMUSIC12.png")
     pfp = Image.open(pic).convert("RGBA")
-    pfp = circle(pfp, brightness_factor=brightness_factor)
-    pfp = pfp.resize((892, 880))
+    pfp = circle(pfp)
+    pfp = pfp.resize((1157, 1158))
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype("VIPMUSIC/assets/font.ttf", size=95)
-    welcome_font = ImageFont.truetype("VIPMUSIC/assets/font.ttf", size=45)
-
-    # Draw user's name with shining red fill and dark saffron border
-    draw.text((1770, 1015), f": {user}", fill=(255, 0, 0), font=font)
-    draw.text(
-        (1770, 1015),
-        f": {user}",
-        fill=None,
-        font=font,
-        stroke_fill=(255, 153, 51),
-        stroke_width=6,
-    )
-
-    # Draw user's id with shining blue fill and white border
-    draw.text((1530, 1230), f": {id}", fill=(0, 0, 139))
-    draw.text(
-        (1530, 1230),
-        f": {id}",
-        fill=None,
-        font=font,
-        stroke_fill=(255, 255, 255),
-        stroke_width=0,
-    )
-
-    # Draw user's username with white fill and green border
-    draw.text((2030, 1450), f": {uname}", fill=(255, 255, 255), font=font)
-    draw.text(
-        (2030, 1450),
-        f": {uname}",
-        fill=None,
-        font=font,
-        stroke_fill=(0, 128, 0),
-        stroke_width=6,
-    )
-
-    # Resize photo and position
-    pfp_position = (255, 323)
+    font = ImageFont.truetype('VIPMUSIC/assets/font.ttf', size=110)
+    welcome_font = ImageFont.truetype('VIPMUSIC/assets/font.ttf', size=60)
+    draw.text((1800, 700), f'NAME: {user}', fill=(255, 255, 255), font=font)
+    draw.text((1800, 830), f'ID: {id}', fill=(255, 255, 255), font=font)
+    draw.text((1800, 965), f"USERNAME : {uname}", fill=(255, 255, 255), font=font)
+    pfp_position = (391, 336)
     background.paste(pfp, pfp_position, pfp)
-
-    # Calculate circular outline coordinates
-    center_x = pfp_position[0] + pfp.width / 2
-    center_y = pfp_position[1] + pfp.height / 2
-    radius = min(pfp.width, pfp.height) / 2
-
-    # Draw circular outlines
-    draw.ellipse(
-        [
-            (center_x - radius - 10, center_y - radius - 10),
-            (center_x + radius + 10, center_y + radius + 10),
-        ],
-        outline=(255, 153, 51),
-        width=25,
-    )  # Saffron border
-
-    draw.ellipse(
-        [
-            (center_x - radius - 20, center_y - radius - 20),
-            (center_x + radius + 20, center_y + radius + 20),
-        ],
-        outline=(255, 255, 255),
-        width=25,
-    )  # White border
-
-    draw.ellipse(
-        [
-            (center_x - radius - 30, center_y - radius - 30),
-            (center_x + radius + 30, center_y + radius + 30),
-        ],
-        outline=(0, 128, 0),
-        width=25,
-    )  # Green border
-
     background.save(f"downloads/welcome#{id}.png")
     return f"downloads/welcome#{id}.png"
 
-
-@Client.on_message(filters.command("welcome") & ~filters.private)
-async def auto_state(client: Client, message):  # Added 'message' as a parameter
+@app.on_message(filters.command("welcome") & ~filters.private)
+async def auto_state(_, message):
     usage = "**ᴜsᴀɢᴇ:**\n**⦿ /welcome [on|off]**"
     if len(message.command) == 1:
         return await message.reply_text(usage)
     chat_id = message.chat.id
-    user = await client.get_chat_member(message.chat.id, message.from_user.id)
+    user = await app.get_chat_member(message.chat.id, message.from_user.id)
     if user.status in (
         enums.ChatMemberStatus.ADMINISTRATOR,
         enums.ChatMemberStatus.OWNER,
@@ -176,12 +109,10 @@ async def auto_state(client: Client, message):  # Added 'message' as a parameter
         await message.reply("**sᴏʀʀʏ ᴏɴʟʏ ᴀᴅᴍɪɴs ᴄᴀɴ ᴇɴᴀʙʟᴇ ᴡᴇʟᴄᴏᴍᴇ ɴᴏᴛɪғɪᴄᴀᴛɪᴏɴ!**")
 
 
-@Client.on_chat_member_updated(filters.group, group=-3)
-async def greet_new_member(
-    client: Client, member: ChatMemberUpdated
-):  # Added 'client' and 'member' as parameters
+@app.on_chat_member_updated(filters.group, group=-8)
+async def greet_new_member(_, member: ChatMemberUpdated):
     chat_id = member.chat.id
-    count = await client.get_chat_members_count(chat_id)
+    count = await app.get_chat_members_count(chat_id)
     A = await wlcm.find_one(chat_id)
     if A:
         return
@@ -192,7 +123,7 @@ async def greet_new_member(
     if member.new_chat_member and not member.old_chat_member:
 
         try:
-            pic = await client.download_media(
+            pic = await app.download_media(
                 user.photo.big_file_id, file_name=f"pp{user.id}.png"
             )
         except AttributeError:
@@ -208,29 +139,43 @@ async def greet_new_member(
             )
             button_text = "๏ ᴠɪᴇᴡ ɴᴇᴡ ᴍᴇᴍʙᴇʀ ๏"
             add_button_text = "๏ ᴋɪᴅɴᴀᴘ ᴍᴇ ๏"
-            deep_link = f"{user.id}"
-            add_link = f"https://t.me/TG_VC_BOT?startgroup=true"
-            temp.MELCOW[f"welcome-{member.chat.id}"] = await client.send_photo(
+            deep_link = f"tg://openmessage?user_id={user.id}"
+            add_link = f"https://t.me/{app.username}?startgroup=true"
+            temp.MELCOW[f"welcome-{member.chat.id}"] = await app.send_photo(
                 member.chat.id,
                 photo=welcomeimg,
                 caption=f"""
-**❅────✦ ᴡᴇʟᴄᴏᴍᴇ ✦────❅**
-
-▰▰▰▰▰▰▰▰▰▰▰▰▰
-**➻ ɴᴀᴍᴇ »** {user.mention}
-**➻ ɪᴅ »** `{user.id}`
-**➻ ᴜ_ɴᴀᴍᴇ »** @{user.username}
-**➻ ᴛᴏᴛᴀʟ ᴍᴇᴍʙᴇʀs »** {count}
-▰▰▰▰▰▰▰▰▰▰▰▰▰
-
-**❅─────✧❅✦❅✧─────❅**
+**ʜᴇʏ {user.mention} ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ 🏖 {member.chat.title} 🏖 
+✧ ᴜsᴇʀ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ✧**
+━━━━━━━━━━━━━━━━━━━━━
+**✧ ɴᴀᴍᴇ ๏** {user.mention}
+**✧ ɪᴅ ๏** `{user.id}`
+**✧ ᴜ_ɴᴀᴍᴇ ๏** @{user.username}
+**✧ ᴛᴏᴛᴀʟ ᴍᴇᴍʙᴇʀs ๏** {count}
+━━━━━━━━━━━━━━━━━━━━━
 """,
                 reply_markup=InlineKeyboardMarkup(
                     [
-                        [InlineKeyboardButton(button_text, user_id=deep_link)],
+                        [InlineKeyboardButton(button_text, url=deep_link)],
                         [InlineKeyboardButton(text=add_button_text, url=add_link)],
                     ]
                 ),
             )
         except Exception as e:
-            LOGGER.error(e)
+            return
+
+
+__MODULE__ = "Welcome"
+__HELP__ = """
+## Welcome Module
+
+This module handles welcome messages for new members joining a group.
+
+### Commands:
+- `/welcome [on|off]`: Enable or disable welcome notifications in the group.
+
+### Features:
+- Automatically sends a welcome message when a new member joins the group.
+- Allows admins to enable or disable welcome notifications.
+
+"""
