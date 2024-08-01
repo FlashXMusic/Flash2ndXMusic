e535a41eab0f16c026f414f94fe88efe8e3482c3
FlashXMusic <emoji5525522@gmail.com>
2024-08-01 15:28:19 +0630

    Update userbot.py

--- VIPMUSIC/core/userbot.py
+++ VIPMUSIC/core/userbot.py
@@ -66,11 +66,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("THE_VIP_BOY")
-                await self.one.join_chat("THE_VIP_BOY_OP")
-                await self.one.join_chat("TG_FRIENDSS")
-                await self.one.join_chat("VIP_CREATORS")
-                await self.one.join_chat("TheTeamVivek")
+                await self.one.join_chat("seriousvs_version10")
+                await self.one.join_chat("seriousvs_version20")
 
             except:
                 pass
@@ -97,11 +94,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("THE_VIP_BOY")
-                await self.two.join_chat("THE_VIP_BOY_OP")
-                await self.two.join_chat("TG_FRIENDSS")
-                await self.two.join_chat("VIP_CREATORS")
-                await self.two.join_chat("TheTeamVivek")
+                await self.two.join_chat("seriousvs_version10")
+                await self.two.join_chat("seriousvs_version20")
             except:
                 pass
             assistants.append(2)
@@ -122,11 +116,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("THE_VIP_BOY")
-                await self.three.join_chat("THE_VIP_BOY_OP")
-                await self.three.join_chat("TG_FRIENDSS")
-                await self.three.join_chat("VIP_CREATORS")
-                await self.three.join_chat("TheTeamVivek")
+                await self.three.join_chat("seriousvs_version10")
+                await self.three.join_chat("seriousvs_version20")
             except:
                 pass
             assistants.append(3)
@@ -148,11 +139,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("THE_VIP_BOY")
-                await self.four.join_chat("THE_VIP_BOY_OP")
-                await self.four.join_chat("TG_FRIENDSS")
-                await self.four.join_chat("VIP_CREATORS")
-                await self.four.join_chat("TheTeamVivek")
+                await self.four.join_chat("seriousvs_version10")
+                await self.four.join_chat("seriousvs_version20")
             except:
                 pass
             assistants.append(4)
@@ -174,11 +162,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("THE_VIP_BOY")
-                await self.five.join_chat("THE_VIP_BOY_OP")
-                await self.five.join_chat("TG_FRIENDSS")
-                await self.five.join_chat("VIP_CREATORS")
-                await self.five.join_chat("TheTeamVivek")
+                await self.five.join_chat("seriousvs_version10")
+                await self.five.join_chat("seriousvs_version20")
             except:
                 pass
             assistants.append(5)
